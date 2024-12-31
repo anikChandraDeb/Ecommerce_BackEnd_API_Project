697d8369e63e31e81ccf7a37ca2f435c177f00ff
Anik <anik.deb161@gmail.com>
2025-01-01 02:20:41 +0600

    Project Structure

---
+++ app.js
@@ -0,0 +1,43 @@
+const express=require('express');
+const rateLimit=require('express-rate-limit');
+const helmet =require('helmet');
+const mongoSanitize =require('express-mongo-sanitize');
+
+const xss =require('xss-clean');
+const hpp =require('hpp');
+const cors =require('cors');
+const mongoose =require('mongoose');
+const path = require("path");
+const router=require('./src/routes/api')
+const app = express();
+
+//Global Application Middleware
+app.use(cors());
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({extended: false}));
+app.use(hpp());
+app.use(helmet());
+app.use(mongoSanitize())
+app.use(xss())
+//Rate Limiter
+const limiter = rateLimit({windowMs:15*60*1000,max:3000});
+app.use(limiter);
+
+//Web Caching Mechanism
+app.set('etag',false);
+//MongoDB Connection
+const CONNECTION='';
+mongoose.connect(CONNECTION)
+.then((result)=>{
+    console.log('MongoDB Connected');
+}).catch((err)=>{
+    console.log(err);
+})
+
+//Set API Route
+app.use('/api',router);
+
+//Set Application Storage
+app.use(express.static('storage'));
+
+module.exports=app;
